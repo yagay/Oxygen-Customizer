c2e495f497d1342a595db71a1acb117a42889f4f
Jie <1614094+yagay@users.noreply.github.com>
2026-09-24 19:28:09 +0800

    Use robust buffered SAF file copying

--- app/src/main/java/it/dhd/oxygencustomizer/utils/FileUtil.java
+++ app/src/main/java/it/dhd/oxygencustomizer/utils/FileUtil.java
@@ -123,21 +123,16 @@ public class FileUtil {
             returnCursor.moveToFirst();
             String name = returnCursor.getString(nameIndex);
             file = new File(getAppContext().getFilesDir(), name);
-            @SuppressLint("Recycle") InputStream inputStream = getAppContext().getContentResolver().openInputStream(uri);
-            FileOutputStream outputStream = new FileOutputStream(file);
-            int read;
-            int maxBufferSize = 1024 * 1024;
-
+            InputStream inputStream = getAppContext().getContentResolver().openInputStream(uri);
             if (inputStream == null) return null;
 
-            int bytesAvailable = inputStream.available();
-            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            final byte[] buffers = new byte[bufferSize];
-            while ((read = inputStream.read(buffers)) != -1) {
-                outputStream.write(buffers, 0, read);
+            try (inputStream; FileOutputStream outputStream = new FileOutputStream(file)) {
+                byte[] buffer = new byte[64 * 1024];
+                int read;
+                while ((read = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, read);
+                }
             }
-            inputStream.close();
-            outputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
