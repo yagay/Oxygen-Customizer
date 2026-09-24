d3aa0618b6038dc3ade9d4ee28ef41b3c3199502
Jie <1614094+yagay@users.noreply.github.com>
2026-09-24 19:30:03 +0800

    Protect system remount state in settings icon compiler

--- app/src/main/java/it/dhd/oxygencustomizer/utils/overlay/compiler/SettingsIconsCompiler.java
+++ app/src/main/java/it/dhd/oxygencustomizer/utils/overlay/compiler/SettingsIconsCompiler.java
@@ -175,15 +175,18 @@ public class SettingsIconsCompiler {
             }
 
             if (mForce) {
-                // Move to system overlay dir
+                // Move to system overlay dir and always restore read-only state.
                 mountRW();
-                for (int i = 1; i <= mPackages.size(); i++) {
-                    Shell.cmd(
-                            "cp -rf " + ModuleConstants.SIGNED_DIR + "/OxygenCustomizerComponentSIP" + i + ".apk " + ModuleConstants.SYSTEM_OVERLAY_DIR + "/OxygenCustomizerComponentSIP" + i + ".apk"
-                    ).exec();
-                    setPermissions(644, "/system/product/overlay/OxygenCustomizerComponentSIP" + i + ".apk");
+                try {
+                    for (int i = 1; i <= mPackages.size(); i++) {
+                        Shell.cmd(
+                                "cp -rf " + ModuleConstants.SIGNED_DIR + "/OxygenCustomizerComponentSIP" + i + ".apk " + ModuleConstants.SYSTEM_OVERLAY_DIR + "/OxygenCustomizerComponentSIP" + i + ".apk"
+                        ).exec();
+                        setPermissions(644, "/system/product/overlay/OxygenCustomizerComponentSIP" + i + ".apk");
+                    }
+                } finally {
+                    mountRO();
                 }
-                mountRO();
 
                 // Enable the overlays
                 String[] overlayNames = new String[mPackages.size()];
